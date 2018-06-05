56e9c6e2269662b4dbcfa88ca319400586f691e5
svetlana.pcelinceva <svetlala@gmail.com>
2018-06-05 12:07:36 +0300

    SKA

--- module1/src/test/java/test/automation/school/test/GoogleSearchTest.java
+++ module1/src/test/java/test/automation/school/test/GoogleSearchTest.java
@@ -1,54 +1,56 @@
-//package test.automation.school.test;
-//
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.testng.Assert;
-//import org.testng.annotations.AfterMethod;
-//import org.testng.annotations.BeforeMethod;
-//import org.testng.annotations.Test;
-//
-//import java.util.List;
-//
-//import static org.testng.Assert.assertTrue;
-//
-//public class GoogleSearchTest {
-//    FirefoxDriver driver;
-//
-//    @BeforeMethod
-//    public void setup(){
-//        System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
-//
-//        System.setProperty("webdriver.gecko.driver", "../drivers/geckodriver-v0.17.0-win32/geckodriver.exe");
-//
-//        driver = new FirefoxDriver();
-//        driver.get("http://www.google.com");
-//    }
-//
-//
-//    @Test
-//    public void testSearchResult(){
-//
-//        WebElement element = driver.findElement(By.name("q"));
-//
-//        element.sendKeys("cheese" + Keys.ENTER);
-//
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        // will find all elements with class='g'
-//        List<WebElement> list =  driver.findElements(By.cssSelector(".g"));
-//        assertTrue(list.size()>0);
-//    }
-//
-//    @AfterMethod
-//    public void teardown(){
-//        driver.quit();
-//    }
-//
-//}
+package test.automation.school.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class GoogleSearchTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "../drivers/chromedriver_v2.39-win32/chromedriver.exe");
+        driver = new ChromeDriver(); //ctrl+ alt+v   new ChromeDriver()
+
+        driver.get("http://google.com");
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void googleSearch() throws InterruptedException {
+
+        //  driver.findElement(By.name("q"));  tozhe samoje. po teksu iskat mozhno tolko xpathom
+        WebElement element = driver.findElement(By.cssSelector("[name='q']"));
+        // driver.findElement(By.cssSelector("[name='q']")); + ctrl+alt+v kogda kursor na dinf Element
+//     driver.findElement(By.xpath("//*[@name='q']"));
+
+        element.sendKeys("cheese!");
+        element.sendKeys(Keys.ENTER);
+
+        Thread.sleep(3000);
+
+        List<WebElement> searchResultsElements = driver.findElements(By.className("g"));
+        assertTrue(searchResultsElements.size()>0);
+
+
+    }
+}
+
+
+
+
